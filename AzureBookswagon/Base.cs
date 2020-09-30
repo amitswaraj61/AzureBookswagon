d5fe98730a0b8b65e01594cdacf617b39f662c65
amitswaraj61 <aswaraj78@gmail.com>
2020-09-30 13:10:11 +0530

    Add project files.

---
+++ AzureBookswagon/Base.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Configuration;
+
+namespace AzureBookswagon
+{
+   public class Base
+    {
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Initilize()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--disable-notifications");
+            driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Manage().Window.Maximize();
+            driver.Url = ConfigurationManager.AppSettings["URL"];
+        }
+
+        [OneTimeTearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
+
---
+++ AzureBookswagon/BookSwagonTest/Test.cs
@@ -0,0 +1,21 @@
+﻿using AzureBookswagon.Pages;
+using NUnit.Framework;
+using System;
+using System.Configuration;
+
+namespace AzureBookswagon.BookSwagonTest
+{
+   public class Test : Base
+    {
+        [Obsolete]
+        [Test]
+        public void ValidLogin()
+        {
+            Login login = new Login(driver);
+            login.LoginToBookSwagaon(ConfigurationManager.AppSettings["email"], ConfigurationManager.AppSettings["password"]);
+            string actualResult = driver.Url;
+            string expectedResult = "https://www.bookswagon.com/myaccount.aspx";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
